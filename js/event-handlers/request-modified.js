51c2f12fe70a8e1afa6e5012508dd36239f4a849
Pavol Starek <pavol@starek.sk>
2015-07-23 13:00:02 +0200

    Linted event handler, linting rules
    - small error handling in requests event handler
    - eslint rules added
    - event handler linted by rules

--- js/event-handlers/request-modified.js
+++ js/event-handlers/request-modified.js
@@ -2,13 +2,12 @@
 	'use strict';
 
 	var log = require(process.cwd() + '/build/server/logging.js').getLogger('handlers/RequestChangedHandler.js');
-	var objectTools = require(process.cwd() + '/build/server/ObjectTools.js');
 	var universalDaoModule = require(process.cwd() + '/build/server/UniversalDao.js');
-	var dateUtils = require(process.cwd()+'/build/server/DateUtils.js').DateUtils;
-	var QueryFilter = require(process.cwd()+'/build/server/QueryFilter.js');
+	var dateUtils = require(process.cwd() + '/build/server/DateUtils.js').DateUtils;
+	var QueryFilter = require(process.cwd() + '/build/server/QueryFilter.js');
 
 	var nodemailer = module.parent.require('nodemailer');
-	var renderModule =  module.parent.require('./renderService.js');
+	var renderModule = module.parent.require('./renderService.js');
 	var transport = nodemailer.createTransport('Sendmail');
 
 	var GEN_REQ_COLLECTION = 'generalRequests';
@@ -23,8 +22,8 @@
 	*	@class RequestChangedHandler
 	*/
 	function RequestChangedHandler(ctx) {
-		this.ctx=ctx;
-		var self=this;
+		this.ctx = ctx;
+		var self = this;
 		var renderService = new renderModule.RenderService();
 
 
@@ -45,115 +44,110 @@
 			@method handleRequestCreated
 
 		*/
-		this.handleRequestCreated=function(event, collection){
+		this.handleRequestCreated = function(event, collection) {
 
 			var entity = event.entity;
-			var solverAddress=this.ctx.config.mails.requestSolverAddress;
+			var solverAddress = this.ctx.config.mails.requestSolverAddress;
 
 			var requestsDao = new universalDaoModule.UniversalDao(
 			this.ctx.mongoDriver,
 			{collectionName: collection}
 			);
-			
+
 			// console.log('mmmmmmmmmmmmmm: ' + JSON.stringify(event));
 
-			if(!entity.requestData){
+			if(!entity.requestData) {
 				entity.requestData = {};
 			}
-			entity.requestData.setupDate=dateUtils.nowToReverse();
-			entity.requestData.status='created';
-			entity.requestData.applicant={schema:"uri://registries/people#views/fullperson-km/view",registry:'people',oid:event.user.id};
+			entity.requestData.setupDate = dateUtils.nowToReverse();
+			entity.requestData.status = 'created';
+			entity.requestData.applicant = {schema: 'uri://registries/people#views/fullperson-km/view', registry: 'people', oid: event.user.id};
 
 			if (event.user && event.user.officer && event.user.officer.club) {
 				entity.requestData.clubApplicant = {
-					schema: "uri://registries/organizations#views/club-km/view",
+					schema: 'uri://registries/organizations#views/club-km/view',
 					oid: event.user.officer.club.oid
 				};
 			} else {
 				log.warn('Applicant %s does not have assigned club as officer but should have', event.user.id);
 			}
-				
-			var qf=QueryFilter.create();
-			qf.addCriterium("systemCredentials.login.email","eq",solverAddress);
+
+			var qf = QueryFilter.create();
+			qf.addCriterium('systemCredentials.login.email', 'eq', solverAddress);
 
 			userDao.find(qf, function(err, data) {
-				if (err){
+				if (err) {
 					log.error(err);
 					return;
 				}
 
 				// assign to and send mail.
-				if (data.length==1){
-					var solver=data[0];
-					entity.requestData.assignedTo={schema:"uri://registries/people#views/fullperson-km/view",registry:'people',oid:solver.id};
+				if (data.length === 1) {
+					var solver = data[0];
+					entity.requestData.assignedTo = {schema: 'uri://registries/people#views/fullperson-km/view', oid: solver.id};
+				} else {
+					log.warn('Failed to find solver with configured email %s in database, request left withoud solver', solverAddress);
 				}
-				self.sendRequestCreated(solverAddress,self.ctx.config.webserverPublicUrl,event.user.baseData.name.v+' '+event.user.baseData.surName.v,entity.requestData.subject,self.ctx.config.serviceUrl+'/requests/'+entity.id);
-				
-				requestsDao.save(entity,function(err,data){
-					if (err) {
-						log.error(err);
+				self.sendRequestCreated(solverAddress, self.ctx.config.webserverPublicUrl, event.user.baseData.name.v + ' ' + event.user.baseData.surName.v, entity.requestData.subject, self.ctx.config.serviceUrl + '/requests/' + entity.id);
+
+				requestsDao.save(entity, function(err2) {
+					if (err2) {
+						log.error(err2);
 						return;
 					}
 					log.debug('requests created: event handled');
 				});
 			});
-			
-
-			
-
 		};
 
 		/**
 			Method handles <b>event-request-updated</b>
 			@method handleRequestModified
 		*/
-		this.handleRequestModified=function(event){
+		this.handleRequestModified = function(event) {
 
 			var entity = event.entity;
 			// var solverAddress=this.ctx.config.mails.requestSolverAddress;
 
 			// if (entity.requestData) {
 
-			entity.requestData.applicant={schema:"uri://registries/people#views/fullperson/view",registry:'people',oid:event.user.id};
-			var solverAddress=self.ctx.config.mails.requestSolverAddress;
-
-				if (entity.requestData.assignedTo){
-					userDao.get(entity.requestData.assignedTo.oid, function(err, solver) {
-						if (err){
+			entity.requestData.applicant = {schema: 'uri://registries/people#views/fullperson/view', registry: 'people', oid: event.user.id};
+			if (entity.requestData.assignedTo) {
+				userDao.get(entity.requestData.assignedTo.oid, function(err, solver) {
+						if (err) {
 							log.error(err);
 							return;
 						}
 
 						// assign to and send mail.
-						if (solver){
-							self.sendRequestModified(solver.systemCredentials.login.email,self.ctx.config.webserverPublicUrl,event.user.baseData.name.v+' '+event.user.baseData.surName.v,entity.requestData.subject,self.ctx.config.serviceUrl+'/requests/'+entity.id);
+						if (solver) {
+							self.sendRequestModified(solver.systemCredentials.login.email, self.ctx.config.webserverPublicUrl, event.user.baseData.name.v + ' ' + event.user.baseData.surName.v, entity.requestData.subject, self.ctx.config.serviceUrl + '/requests/' + entity.id);
 						}
 					});
 
-				} else {
-					self.sendRequestModified(this.ctx.config.mails.requestSolverAddress,self.ctx.config.webserverPublicUrl,event.user.baseData.name.v+' '+event.user.baseData.surName.v,entity.requestData.subject,self.ctx.config.serviceUrl+'/requests/'+entity.id);
-				}
+			} else {
+				self.sendRequestModified(this.ctx.config.mails.requestSolverAddress, self.ctx.config.webserverPublicUrl, event.user.baseData.name.v + ' ' + event.user.baseData.surName.v, entity.requestData.subject, self.ctx.config.serviceUrl + '/requests/' + entity.id);
+			}
 
-				if (entity.requestData.applicant){
-					userDao.get(entity.requestData.applicant.oid,function(err,applicant){
-						if (err){
+			if (entity.requestData.applicant) {
+				userDao.get(entity.requestData.applicant.oid, function(err, applicant) {
+						if (err) {
 							log.error(err);
 							return;
 						}
-						self.sendRequestModified(applicant.systemCredentials.login.email,self.ctx.config.webserverPublicUrl,event.user.baseData.name.v+' '+event.user.baseData.surName.v,entity.requestData.subject,self.ctx.config.serviceUrl+'/requests/'+entity.id);
+						self.sendRequestModified(applicant.systemCredentials.login.email, self.ctx.config.webserverPublicUrl, event.user.baseData.name.v + ' ' + event.user.baseData.surName.v, entity.requestData.subject, self.ctx.config.serviceUrl + '/requests/' + entity.id);
 					});
-				}
+			}
 			// }
-		}
+		};
 
 
-		this.sendRequestCreated=function(email,serviceUrl,applicant,subject,requestUri){
-
+		this.sendRequestCreated = function(email, serviceUrl, applicant, subject, requestUri) {
 			var mailOptions = {
-				from : 'websupport@unionsoft.sk',
-				to : email,
-				subject : '['+serviceUrl+'] Nová žiadosť',
-				html : renderService.render(renderModule.templates.REQUEST_CREATED_HTML,{applicant:applicant,subject:subject,serviceUrl:serviceUrl,requestUri:requestUri})
+				from: 'websupport@unionsoft.sk',
+				to: email,
+				subject: '[' + serviceUrl + '] Nová žiadosť',
+				html: renderService.render(renderModule.templates.REQUEST_CREATED_HTML, {applicant: applicant, subject: subject, serviceUrl: serviceUrl, requestUri: requestUri})
 			};
 
 			log.verbose('Sending mail ', mailOptions);
@@ -162,13 +156,13 @@
 
 
 		};
-		this.sendRequestModified=function(email,serviceUrl,modifier,subject,requestUri){
+		this.sendRequestModified = function(email, serviceUrl, modifier, subject, requestUri) {
 
 			var mailOptions = {
-				from : 'websupport@unionsoft.sk',
-				to : email,
-				subject : '['+serviceUrl+'] Upravená žiadosť',
-				html : renderService.render(renderModule.templates.REQUEST_UPDATED_HTML,{modifier:modifier,subject:subject,serviceUrl:serviceUrl,requestUri:requestUri})
+				from: 'websupport@unionsoft.sk',
+				to: email,
+				subject: '[' + serviceUrl + '] Upravená žiadosť',
+				html: renderService.render(renderModule.templates.REQUEST_UPDATED_HTML, {modifier: modifier, subject: subject, serviceUrl: serviceUrl, requestUri: requestUri})
 			};
 
 			log.verbose('Sending mail ', mailOptions);
@@ -184,30 +178,30 @@
 	* @method handle
 	*/
 	RequestChangedHandler.prototype.handle = function(event) {
-		log.info('handle called',event,RequestChangedHandler.prototype.ctx);
+		log.info('handle called', event, RequestChangedHandler.prototype.ctx);
 
-		if ('event-general-request-created' === event.eventType){
+		if (event.eventType === 'event-general-request-created' === event.eventType) {
 			this.handleRequestCreated(event, GEN_REQ_COLLECTION);
-		}else if('event-registration-request-created' === event.eventType){
+		}else if(event.eventType === 'event-registration-request-created' === event.eventType) {
 			this.handleRequestCreated(event, REG_REQ_COLLECTION);
-		}else if('event-data-request-created' === event.eventType){
+		}else if(event.eventType === 'event-data-request-created' === event.eventType) {
 			this.handleRequestCreated(event, DATA_REQ_COLLECTION);
-		}else if('event-transfer-request-created' === event.eventType){
+		}else if(event.eventType === 'event-transfer-request-created' === event.eventType) {
 			this.handleRequestCreated(event, TRANS_REQ_COLLECTION);
-		}else if ('event-general-request-updated' === event.eventType){
+		}else if (event.eventType === 'event-general-request-updated' === event.eventType) {
 			this.handleRequestModified(event, GEN_REQ_COLLECTION);
-		}else if('event-registration-request-updated' === event.eventType){
+		}else if(event.eventType === 'event-registration-request-updated' === event.eventType) {
 			this.handleRequestModified(event, REG_REQ_COLLECTION);
-		}else if('event-data-request-updated' === event.eventType){
-			this.handleRequestModified(event, DATA_REQ_COLLECTION)
-		}else if('event-transfer-request-updated' === event.eventType){
+		}else if(event.eventType === 'event-data-request-updated' === event.eventType) {
+			this.handleRequestModified(event, DATA_REQ_COLLECTION);
+		}else if(event.eventType === 'event-transfer-request-updated') {
 			this.handleRequestModified(event, TRANS_REQ_COLLECTION);
 		}
 	};
 
-	RequestChangedHandler.prototype.getType=function(){
-		return ["event-general-request-created","event-registration-request-created","event-data-request-created", "event-transfer-request-created",
-				"event-general-request-updated","event-registration-request-updated","event-data-request-updated", "event-transfer-request-updated"];
+	RequestChangedHandler.prototype.getType = function() {
+		return ['event-general-request-created', 'event-registration-request-created', 'event-data-request-created', 'event-transfer-request-created',
+				'event-general-request-updated', 'event-registration-request-updated', 'event-data-request-updated', 'event-transfer-request-updated'];
 	};
 
 	module.exports = function( ctx) {
